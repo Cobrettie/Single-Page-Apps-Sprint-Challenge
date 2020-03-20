07d1dc688acf85eb93eb92a27fec0f2386e1d466
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-20 19:15:33 -0400

    add axios request to API inside of useEffect hook, successfully returning data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [listOfCharacters, setListOfCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        const allCharacters = response.data.results;
+        setListOfCharacters(allCharacters)
+      })
   }, []);
 
   return (
