c492cac28134c833fe1a0ffb7f560fe6dc7e5b27
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-21 09:30:20 -0400

    add image for each CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,15 +3,19 @@ import styled from 'styled-components';
 
 const SingleCard = styled.div`
   border: 1px solid #373737;
+  border-radius: 5px;
   width: 30%;
+  margin: 20px;
+
 `;
 
 export default function CharacterCard({ character }) {
-  const { id, name, status, species, type, gender } = character;
+  const { id, name, image, status, species, type, gender } = character;
   // console.log(character);
   return (
     <SingleCard>
       <h2>Name: {name}</h2>
+      <img src={image}></img>
       <p>Status: {status}</p>
       <p>Species: {species}</p>
       <p>Type: {type}</p>
