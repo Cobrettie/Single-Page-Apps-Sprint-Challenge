a0a9385c75e13ff892b479383070134bccd6353a
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-20 20:15:06 -0400

    add basic styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,21 +3,19 @@ import styled from 'styled-components';
 
 const SingleCard = styled.div`
   border: 1px solid #373737;
-  width: 100%;
+  width: 30%;
 `;
 
 export default function CharacterCard({ character }) {
   const { id, name, status, species, type, gender } = character;
   // console.log(character);
   return (
-    <div>
-      <SingleCard>
-        <h2>Name: {name}</h2>
-        <p>Status: {status}</p>
-        <p>Species: {species}</p>
-        <p>Type: {type}</p>
-        <p>Gender: {gender}</p>
-      </SingleCard>
-    </div>
+    <SingleCard>
+      <h2>Name: {name}</h2>
+      <p>Status: {status}</p>
+      <p>Species: {species}</p>
+      <p>Type: {type}</p>
+      <p>Gender: {gender}</p>
+    </SingleCard>
   );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  border-bottom: 2px solid #373737;
+  padding: 20px 0;
+  margin: 0 auto 40px;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: dodgerblue;
+  padding: 0 20px;
+`;
 
 export default function NavMenu() {
   return (
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='characterlist'>List of characters</Link>
-    </nav>
+    <StyledNav>
+      <StyledNavLink to='/'>Home</StyledNavLink>
+      <StyledNavLink to='characterlist'>List of characters</StyledNavLink>
+    </StyledNav>
   )
 }
