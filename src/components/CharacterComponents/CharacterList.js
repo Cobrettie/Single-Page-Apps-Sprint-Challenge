a4cf0823f64c5e3965ee0f8a9d20b67e27e032b5
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-23 20:34:39 -0400

    add functionality to loop through list continuously

--- src/components/CharacterComponents/CharacterList.js
+++ src/components/CharacterComponents/CharacterList.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-
 import SearchForm from "../SearchForm/SearchForm";
 import CharacterCard from './CharacterCard';
-
 import { CharacterCardsContainer } from "./CharacterStyles";
+
+const charactersAPI = 'https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/';
+const apiPagePrefix = '?page=';
 
 export default function CharacterList() {
   const [listOfCharacters, setListOfCharacters] = useState([]);
   const [apiPageNumber, setApiPageNumber] = useState(1);
-
-  const charactersAPI = 'https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/';
-  const apiPagePrefix = '?page=';
 
   const incrementApiPageNumber = () => {
     return (
@@ -34,8 +32,19 @@ export default function CharacterList() {
         console.log(response)
         const allCharacters = response.data.results;
         setListOfCharacters(allCharacters)
+        if (response.data.info.next === '') {
+          setApiPageNumber(1)
+        }
+        if (response.data.info.prev === '') {
+          setApiPageNumber(1)
+        }
       })
+      .catch(err => console.log(err))
   }, [apiPageNumber]);
+
+  // if (apiPageNumber === 0 || apiPageNumber )
+
+  // if data.info.next or .previous === 'empty string' setApiPageNumber 1, 25
 
   return (
     <div>
