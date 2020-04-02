a85900cece9816092c4ecf2e9ac3571d4fd30109
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-01 20:03:58 -0400

    style EpisodeCard.js through EpisodeStyles.js

--- src/components/EpisodeComponents/EpisodeCard.js
+++ src/components/EpisodeComponents/EpisodeCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { SingleCard } from './EpisodeStyles';
+import { SingleCard, HeaderNameContainer, HeaderNameH2, InfoContainerDiv } from './EpisodeStyles';
+import { CardInfoP } from '../CharacterComponents/CharacterStyles';
 
 export default function EpisodeCard({ episodes }) {
   const {name, air_date, episode, characters } = episodes;
 
   return (
     <SingleCard>
-      <h2>{name}</h2>
+      <HeaderNameContainer>
+        <HeaderNameH2>{name}</HeaderNameH2>
+      </HeaderNameContainer>
+      <InfoContainerDiv>
+        <CardInfoP>Air Date: <span>{air_date}</span></CardInfoP>
+        <CardInfoP>Episode: <span>{episode}</span></CardInfoP>
+      </InfoContainerDiv>
     </SingleCard>
   )
 }
--- src/components/EpisodeComponents/EpisodeStyles.js
+++ src/components/EpisodeComponents/EpisodeStyles.js
@@ -15,3 +15,40 @@ export const SingleCard = styled.div`
   overflow: hidden;
   margin: 20px;
 `;
+
+export const HeaderNameContainer = styled.div`
+  width: 100%;
+  max-width: 100%;
+  opacity: 0.8;
+  background: rgb(32, 35, 41);
+  padding: 0.625rem;
+  text-align: left;
+`;
+
+export const HeaderNameH2 = styled.h2`
+  color: rgb(245, 245, 245);
+  font-size: 1.4rem;
+  font-weight: 400;
+  margin: 0px;
+  max-width: 250px;
+`;
+
+export const InfoContainerDiv = styled.div`
+  background: rgb(51, 51, 51);
+  padding: 1.25rem;
+`;
+
+export const CardInfoP = styled.p`
+  color: rgb(245, 245, 245);
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  border-bottom: 1px solid rgb(68, 68, 68);
+  padding: 0.75rem 0px 0.375rem;
+
+  span {
+    color: #ABD5EC;
+    text-align: right;
+  }
+`;
